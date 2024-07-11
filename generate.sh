9684e1b506cda676494bdcbcd53f8665c52366eb
naoki-p <naokiwatanabe@live.com>
2024-07-11 16:58:19 +0900

    some changes..

--- generate.sh
+++ generate.sh
@@ -19,10 +19,6 @@ podman run -d --pod nc --name redis \
 # NextCloud
 podman run -d --pod nc --name nextcloud \
   -e REDIS_HOST=localhost \
-  -e POSTGRES_HOST=localhost \
-  -e POSTGRES_DB=nextcloud \
-  -e POSTGRES_USER=nextcloud \
-  -e POSTGRES_PASSWORD=nextcloud \
   -v nc-html:/var/www/html \
   docker.io/library/nextcloud:latest
 # Caddy
@@ -33,19 +29,22 @@ podman run -d --pod nc --name caddy \
   docker.io/library/caddy:latest
 # Generate k8s YAML
 podman generate kube nc > nc.yml
+echo "Wait 10 sec. for the containers to start."
+sleep 10
 # Setup
 # see: https://docs.nextcloud.com/server/latest/admin_manual/configuration_server/occ_command.html#command-line-installation
 # see: https://denor.jp/%E3%81%8A%E3%81%86%E3%81%A1lan%E5%86%85%E3%81%AEnextcloud%E3%82%B5%E3%83%BC%E3%83%90%E3%82%92ssl%E8%87%AA%E5%8B%95%E6%9B%B4%E6%96%B0%E3%81%AEcaddy%E3%81%A7%E3%83%9B%E3%82%B9%E3%83%86%E3%82%A3%E3%83%B3
-podman exec -it -u www-data nextcloud ./occ maintenance:install --admin-user "admin" --admin-pass "passwd"
-podman exec -it -u www-data nextcloud ./occ config:system:set overwritehost --value='localhost:8080'
-podman exec -it -u www-data nextcloud ./occ config:system:set overwriteprotocol --value='http'
-podman exec -it -u www-data nextcloud ./occ config:system:set overwritewebroot --value='/nextcloud'
-podman exec -it -u www-data nextcloud ./occ config:system:set overwritecondaddr --value='^localhost:8080$'
-podman exec -it -u www-data nextcloud ./occ config:system:set overwrite.cli.url --value='http://localhost:8080'
-podman exec -it -u www-data nextcloud ./occ config:system:set trusted_proxies --value="[localhost]"
+podman exec -it -u www-data nextcloud ./occ maintenance:install --database="pgsql" \
+	--database-name="nextcloud" --database-user="nextcloud" --database-pass="nextcloud"
+podman exec -it -u www-data nextcloud ./occ config:system:set overwritehost --value="localhost:8080"
+podman exec -it -u www-data nextcloud ./occ config:system:set overwriteprotocol --value="http"
+podman exec -it -u www-data nextcloud ./occ config:system:set overwritewebroot --value="/nextcloud"
+podman exec -it -u www-data nextcloud ./occ config:system:set overwritecondaddr --value="^localhost:8080$"
+podman exec -it -u www-data nextcloud ./occ config:system:set overwrite.cli.url --value="http://localhost:8080"
+podman exec -it -u www-data nextcloud ./occ config:system:set trusted_proxies 0 --value="localhost"
+podman exec -it -u www-data nextcloud ./occ db:add-missing-indices
 ## Cleanup
 podman pod rm -f nc
-podman volume rm nc-pgdata
-podman volume rm nc-redisdata
-podman volume rm nc-html
-podman volume rm nc-caddydata
+echo "Let's play the pod."
+echo ""
+echo "$ podman kube play nc.yml"
